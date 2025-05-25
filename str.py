a3e3cae13c5b8efffe2551accce36d215b436134
MAN SINGH BAR <mansinghbar@gmail.com>
2025-05-25 12:54:08 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Man singh")
+st.header("CHOUHAN INVESTMENT POINT")
+
+from PIL import Image
+img=Image.open("m.jpg")
+
+st.image(img, width=200*200,)
+st.subheader("PLEASE call ME 9950182347")
+
